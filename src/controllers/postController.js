5fdbfa540397ef51bf1ad845b0771b7030a99164
Mariana Cardoso <mariana.c.cardoso@aluno.senai.br>
2025-04-15 15:25:39 -0300

    feat: create post PUT method

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -16,13 +16,36 @@ class PostController {
             if (!title || !content || !imageUrl) {
                 return res.status(400).json({ erro: "Todos os campos são obrigatórios" });
             }
-            const newPost = await userModel.create(title, content, imageUrl);
+            const newPost = await postModel.create(title, content, imageUrl);
             res.status(201).json(newPost);
         } catch (error) {
             console.error(error);
             res.status(500).json({ erro: "Erro ao criar postagem" });
         }
     };
+    update = async (req, res) => {
+        const { id } = req.params;
+        const { title, content, imageUrl } = req.body;
+
+        try {
+            const postUpdated = await postModel.update(
+                parseInt(id),
+                title,
+                content,
+                imageUrl
+            );
+
+            if (!postUpdated) {
+                return res.status(404).json({ erro: "Postagem não encontradaa" });
+            }
+
+            res.json(postUpdated);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ erro: "Erro ao atualizar postagem" });
+        }
+    };
+
 
 
 }
