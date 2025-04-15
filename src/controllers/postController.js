06ea9500a4e1b5a42d19af3d7b84c308c8a756a9
Mariana Cardoso <mariana.c.cardoso@aluno.senai.br>
2025-04-15 15:22:15 -0300

    feat: create post POST method

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -10,7 +10,19 @@ class PostController {
             res.status(500).json({ erro: "Erro ao buscar postagens" });
         }
     };
-    
+    create = async (req, res) => {
+        const { title, content, imageUrl } = req.body;
+        try {
+            if (!title || !content || !imageUrl) {
+                return res.status(400).json({ erro: "Todos os campos são obrigatórios" });
+            }
+            const newPost = await userModel.create(title, content, imageUrl);
+            res.status(201).json(newPost);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ erro: "Erro ao criar postagem" });
+        }
+    };
 
 
 }
